3fe681224bd6d11a633114142a6197644a7b5446
Daniel Caldera <bdcaldera@gmail.com>
2019-08-18 18:14:42 -0300

    Inicializacion del proyecto Js - Html

---
+++ dom/codigo.js
@@ -0,0 +1,10 @@
+//alert('Sí funciona');
+var ruta = window.location;
+console.log(ruta);
+document.write("Estás aquí:" + ruta.origin);
+var ruta2 = navigator.location;
+console.log(ruta2);
+var ruta3 = document.location;
+console.log(ruta3);
+var ruta4 = console.location;
+console.log(ruta4);
