62a0a5f7a0cc1361a2c0193a0f25773a3332cd65
Daniel Caldera <bdcaldera@gmail.com>
2019-08-18 22:13:05 -0300

    Se realizaron cambios en los documentos html de ejemplo

--- document.js
+++
@@ -1,9 +0,0 @@
-var ruta = window.location;
-console.log(ruta);
-document.write("Estás aquí:" + ruta.origin);
-var ruta2 = navigator.location;
-console.log(ruta2);
-var ruta3 = document.location;
-console.log(ruta3);
-var ruta4 = console.location;
-console.log(ruta4);
